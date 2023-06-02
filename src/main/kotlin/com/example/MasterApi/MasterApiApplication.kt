cb53c9d2047189edc86444f8e58657ca80f6162e
marcelo.d.amorim <marcelo.d.amorim@accenture.com>
2023-06-01 22:31:30 -0300

    primeiro commit

---
+++ src/main/kotlin/com/example/MasterApi/MasterApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.MasterApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MasterApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MasterApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/MasterApi/MasterApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.MasterApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MasterApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
